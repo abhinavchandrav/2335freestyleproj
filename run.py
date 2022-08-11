3043fdc7063574dbc7707dd9e8d6004973a53a24
R1425 <108553492+R1425@users.noreply.github.com>
2022-08-10 20:34:53 -0400

    Updated test for clarity

--- run.py
+++ run.py
@@ -9,15 +9,20 @@ import pandas as pd
 
 def input_stock(symbol):
     api_key = os.getenv("api_key")
+    
+    # Treasury Yield Data 
     t_csv_filepath = f"https://www.alphavantage.co/query?function=TREASURY_YIELD&interval=daily&maturity=1year&apikey={api_key}&datatype=csv"
     t_df = read_csv(t_csv_filepath)
+
+    # Stock Data 
     csv_filepath = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol.upper()}&outputsize=full&apikey={api_key}&datatype=csv"
     df = read_csv(csv_filepath)
+    
     try:
         latest_close = df.iloc[0]["adjusted_close"]
         stock_momentum_report(df,t_df,symbol)
 
     except KeyError:
-        print("OPPS! That ticker is not available.  Please check your symbol and try again.")
-        return "OPPS! That ticker is not available.  Please check your symbol and try again."
+        print("The ticker you have entered is not a valid ticker.  Please check your ticker and try again.")
+        return "The ticker you have entered is not a valid ticker.  Please check your ticker and try again."
     return "Valid"
--- test_input.py
+++ test_input.py
@@ -1,5 +1,8 @@
+# This tests the user input which in turn tests the API 
+# A data point that comes in through the API is part of the input_stock function
+
 from run import input_stock
 
 def test_input():
     assert input_stock('NFLX') == "Valid"
-    assert input_stock('XYABC') == "OPPS! That ticker is not available.  Please check your symbol and try again."
+    assert input_stock('XYABC') == "The ticker you have entered is not a valid ticker.  Please check your ticker and try again."
